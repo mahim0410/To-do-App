83037d6d3d7ccdcfcb4638d7db8fdfefeb9eb18b
mahim0410 <mahidulislam3397@gmail.com>
2026-03-03 01:17:08 +0600

    Update added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { NoteCard } from '../components/NoteCard'
 import api from '../lib/axios'
+import NotesNotFound from '../components/NotesNotFound'
+
+
 
 const HomePage = () => {
 
@@ -30,6 +33,8 @@ const HomePage = () => {
             <Navbar />
             <div className="max-w-7xl mx-auto p-4 mt-6">
                 {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+                {!loading && notes.length === 0 && <NotesNotFound />}
+
                 {notes.length > 0 && true && (
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                         {notes.map((note) => {
